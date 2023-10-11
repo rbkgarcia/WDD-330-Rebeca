2f611f0503e834244914306076594a9113da8ba8
rbkgarcia <gar19066@byui.edu>
2023-10-11 18:27:18 -0400

    Team Activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,6 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
- let currentCart = getLocalStorage("so-cart") || [];
-  currentCart.push(product) 
-  setLocalStorage("so-cart", currentCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productId = getParam("product");
+productDetails(productId);
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
